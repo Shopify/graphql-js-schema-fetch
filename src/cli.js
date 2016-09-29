54acdb600cc2870b9be1e464b3469cf8081f2757
Mina Smart <mina.smart@shopify.com>
2016-09-29 13:05:09 -0400

    Add help message

--- src/cli.js
+++ src/cli.js
@@ -2,8 +2,14 @@
 
 import parseArgs from './parse-args';
 import fetchSchema from './index';
+import help from './help';
 
 const args = parseArgs(process.argv.slice(2));
+
+if (args.showHelp) {
+  console.log(help);
+  process.exit(0);
+}
 
 fetchSchema(args.url, args.method, args.headers).then((schema) => {
   console.log(JSON.stringify(schema));
---
+++ src/help.js
@@ -0,0 +1,11 @@
+export default `
+usage: graphql-js-schema-fetch --url my-api.com [--method=POST] [--header="...", --header="..."]
+
+Fetch the json representation of a GraphQL Schema from a live server.
+
+arguments:
+ --url               The URL where you GraphQL API resides.
+ --method            The HTTP method to use during the fetch (default: 'POST')
+ --header[,--header] Any headers to send along with the request. Specify this
+                     option multiple times for multiple headers.
+`;
