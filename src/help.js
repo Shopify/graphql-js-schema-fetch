b9be24a0638837f0c70577f45d404d2f1bdc8a36
Mina Smart <mina.smart@shopify.com>
2016-09-29 13:12:27 -0400

    Add header examples

--- src/help.js
+++ src/help.js
@@ -7,5 +7,6 @@ arguments:
  --url               The URL where you GraphQL API resides.
  --method            The HTTP method to use during the fetch (default: 'POST')
  --header[,--header] Any headers to send along with the request. Specify this
-                     option multiple times for multiple headers.
+                     option multiple times for multiple headers. (example:
+                     --header "Authorization: Basic abc123" --header "X-Version: 1")
 `;
