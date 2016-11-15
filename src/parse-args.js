9376e2973582c459a3ba3ba09ce2e3bf9ae76a7a
mikkoh <mikko.haapoja@shopify.com>
2016-11-15 09:53:30 -0500

    Now we ensure at least headers for Accept and Content-Type exist

--- src/parse-args.js
+++ src/parse-args.js
@@ -9,21 +9,18 @@ export default function parseArgs(rawArgs) {
 
   const url = args.url;
   const method = args.method;
+  const header = args.header || [];
 
-  let headers = [];
+  const headers = [].concat(header).reduce((headerAcc, argHeader) => {
+    const pair = argHeader.split(':');
 
-  if (args.header) {
-    headers = [].concat(args.header).reduce((headerAcc, header) => {
-      const pair = header.split(':');
+    headerAcc[pair[0].trim()] = pair[1].trim();
 
-      headerAcc[pair[0].trim()] = pair[1].trim();
-
-      return headerAcc;
-    }, {
-      Accept: 'application/json',
-      'Content-Type': 'application/x-www-form-urlencoded'
-    });
-  }
+    return headerAcc;
+  }, {
+    Accept: 'application/json',
+    'Content-Type': 'application/x-www-form-urlencoded'
+  });
 
   return {url, method, headers};
 }
